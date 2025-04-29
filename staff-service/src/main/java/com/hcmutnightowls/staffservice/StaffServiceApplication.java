3efe47bda9ab236e2b4e85b2363edc83023f3afb
Phuc Tran Hong <tranhongphucfb@gmail.com>
2025-04-29 19:52:31 +0700

    feat: add dotenv dependency and update database configuration properties

--- staff-service/src/main/java/com/hcmutnightowls/staffservice/StaffServiceApplication.java
+++ staff-service/src/main/java/com/hcmutnightowls/staffservice/StaffServiceApplication.java
@@ -1,12 +1,20 @@
 package com.hcmutnightowls.staffservice;
 
+import java.nio.file.Paths;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class StaffServiceApplication {
 
     public static void main(String[] args) {
+        // System.out.println("Current directory: " + Paths.get(".").toAbsolutePath().normalize());
+        Dotenv dotenv = Dotenv.configure()
+                .load();
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
         SpringApplication.run(StaffServiceApplication.class, args);
     }
 
